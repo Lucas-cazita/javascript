eca2b86d36fddcb69d09a7762331af156b1ba6b4
Lucas-cazita <lucascazita@gmail.com>
2024-11-23 01:19:29 +0000

    criei o relogio-ibr

--- relogio-ibr/script.js
+++ relogio-ibr/script.js
@@ -49,22 +49,111 @@ function save() {
 }
 
 save()
-
-// Programação Atual
-function atualProg(prog) {
-    document.querySelector('h1').innerHTML = allProgs[0][prog].toUpperCase()
-    document.querySelector('.next').innerHTML = `/${allProgs[0][prog + 1]}`
-}
-
+let runCount = true
+const timers = { interval: null };
 // Inicia a Progamação
 function start() {
-    let progAtual = [allProgs[0][0], allProgs[1][0]]
-    console.log()
-    atualProg(progAtual[0])
+    const h1 = document.querySelector('h1')
+    const next = document.querySelector('.next')
+
+    let index = 0
+    runCount = true
+
+    function atualizaProgramação () {
+        if (!runCount) return
+
+        if (index < allProgs[0].length) {
+            h1.textContent = allProgs[0][index].toUpperCase()
+            if (allProgs[0][index + 1]) {
+                next.textContent = allProgs[0][index + 1]
+            } else {
+                next.textContent = 'FIM'
+            }
+
+            const tempoAtual = allProgs[1][index] * 60
+
+            startContdown(tempoAtual)
+
+            index++
+            setTimeout( () => {
+                if (runCount) atualizaProgramação()
+            }, tempoAtual * 1000)
+        } else {
+            stop()
+        }
+    }
+
+    atualizaProgramação()
 }
 
+// Inicia o Timer
+function startContdown(durationInSeconds) {
+    const countdown = document.querySelector('.contdown')
+    const progressBar = document.querySelector('.progressBar')
+    let timeRemaining = durationInSeconds
+
+    const totalDuration = durationInSeconds
 
 
+    function formatTime(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    }
+
+    function updateCountdown() {
+        if (timeRemaining >= 0 && runCount) {
+            countdown.textContent = formatTime(timeRemaining);
+
+            // Calcula a porcentagem de tempo restante
+            const percentage = ((totalDuration - timeRemaining) / totalDuration) * 100;
+
+            // Atualiza a largura da barra de progresso
+            progressBar.style.width = `${percentage}%`;
+            progressBar.style.height = `100%`;
+
+
+            // Calcula a cor (verde -> vermelho)
+            const red = Math.min(255, Math.floor((1 - timeRemaining / totalDuration) * 255));
+            const green = Math.min(255, Math.floor((timeRemaining / totalDuration) * 255));
+            progressBar.style.backgroundColor = `rgb(${red}, ${green}, 0)`;
+
+             // Quando o tempo for menor ou igual a 2 minutos, piscar a barra e os números
+            if (timeRemaining <= 120) {
+                progressBar.classList.add('blink-bar');
+                countdown.classList.add('blink-text');
+            }
+
+            timeRemaining--;
+        } else {
+            countdown.textContent = '00:00'
+            progressBar.classList.remove('blink-bar');
+            countdown.classList.remove('blink-text');
+            clearInterval(timers.interval)
+            stop()
+        }
+    }
+
+    clearInterval(timers.interval); // Garante que não haja múltiplos intervalos ativos
+    timers.interval = setInterval(updateCountdown, 1000); // Armazena no objeto
+    updateCountdown(); // Atualiza imediatamente a primeira vez
+}
+
+function stop() {
+    clearInterval(timers.interval); // Acessa o timer pelo objeto
+    const progressBar = document.querySelector('.progressBar')
+    const h1 = document.querySelector('h1')
+    const next = document.querySelector('.next')
+    const countdown = document.querySelector('.contdown')
+    
+    h1.textContent = '...'
+    next.textContent = '...'
+    progressBar.style.width = 0
+    countdown.textContent = '00:00'
+
+
+    runCount = false
+}
 
 // Evento abrir side menu
 let btnSideMenu = document.querySelector('.side-btn')
@@ -85,47 +174,43 @@ function openSideMenu() {
 }
 
 // Evento de entrar em Tela Cheia
-const btnResize = document.getElementById('fullscreen-btn')
-const icon = document.getElementById('fullscreen-icon')
+const btnResize = document.getElementById('fullscreen-btn');
+const icon = document.getElementById('fullscreen-icon');
+
 btnResize.addEventListener('click', () => {
-    let fullScreen;
     if (!document.fullscreenElement) {
-        // Entrar no modo tela cheia
-        if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
-        } else if (document.documentElement.webkitRequestFullscreen) {
-            document.documentElement.webkitRequestFullscreen();
-        } else if (document.documentElement.msRequestFullscreen) {
-            document.documentElement.msRequestFullscreen();
-        }
-
-        icon.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <polyline points="15 9 21 9 21 3"></polyline>
-          <polyline points="9 9 3 9 3 3"></polyline>
-          <polyline points="15 15 21 15 21 21"></polyline>
-          <polyline points="9 15 3 15 3 21"></polyline>
-        </svg>`;
-
-        fullScreen = true
+        document.documentElement.requestFullscreen();
+        updateFullscreen(true);
     } else {
-        // Sair do modo tela cheia
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-
-        icon.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <polyline points="9 3 3 3 3 9"></polyline>
-          <polyline points="15 3 21 3 21 9"></polyline>
-          <polyline points="9 21 3 21 3 15"></polyline>
-          <polyline points="15 21 21 21 21 15"></polyline>
-        </svg>`
-
-        fullScreen = false
+        document.exitFullscreen();
+        updateFullscreen(false);
     }
 });
+
+
+function updateFullscreen(isFullscreen) {
+    const controlers = document.querySelector('.controls')
+    icon.innerHTML = isFullscreen
+        ? `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+             <polyline points="15 9 21 9 21 3"></polyline>
+             <polyline points="9 9 3 9 3 3"></polyline>
+             <polyline points="15 15 21 15 21 21"></polyline>
+             <polyline points="9 15 3 15 3 21"></polyline>
+           </svg>`
+        : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+             <polyline points="9 3 3 3 3 9"></polyline>
+             <polyline points="15 3 21 3 21 9"></polyline>
+             <polyline points="9 21 3 21 3 15"></polyline>
+             <polyline points="15 21 21 21 21 15"></polyline>
+           </svg>`;
+
+    btnSideMenu.style.display = isFullscreen ? 'none' : 'block'
+    
+    if (isFullscreen) {
+        controlers.classList.add('hidden');
+        console.log('Entrou em tela cheia');
+    } else {
+        controlers.classList.remove('hidden');
+        console.log('Saiu de tela cheia');
+    }
+}
